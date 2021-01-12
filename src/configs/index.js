c14baf95bfe0a8ff9261917585f96901bb6a6869
vuduyduong <duongvu0953@gmail.com>
2021-01-12 21:25:53 +0700

    add database finished

--- src/configs/index.js
+++ src/configs/index.js
@@ -16,11 +16,11 @@ module.exports = {
   db: {
     client: 'mysql',
     connection: {
-      host: process.env.DB_HOST,
-      database: process.env.DB_NAME,
-      user: process.env.DB_USERNAME,
-      password: process.env.DB_PASSWORD,
-      port: process.env.DB_PORT,
+      host: process.env.DB_HOST || 'lfmerukkeiac5y5w.cbetxkdyhwsb.us-east-1.rds.amazonaws.com',
+      database: process.env.DB_NAME || 'vqdbzx84yowqck55',
+      user: process.env.DB_USERNAME || 'vws4pyx8ihqbww2m',
+      password: process.env.DB_PASSWORD || 'u9t5xs4ai5vm21re',
+      port: process.env.DB_PORT || 3306,
     },
   },
   app: {
